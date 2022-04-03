1ced6ed1ab881b8d1f762cf30a481a8ec610b719
chandan <chandan2000kulu@gmail.com>
2022-04-03 21:37:32 +0530

    i added constructor in news.js and some operation in newsitem.js

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -8,17 +8,17 @@ export class NewsItem extends Component {
 
     render() {
 
-        let { title, description } = this.props;
+        let { title, description, imageUrl, newsUrl } = this.props;
 
         return (
-            <div>
-                <div className="card" style={{width: "18rem"}}>
-                    <img src="https://live-production.wcms.abc-cdn.net.au/cca56ba98abb099854bbd695da8c7822?impolicy=wcms_crop_resize&cropH=2813&cropW=5000&xPos=0&yPos=260&width=862&height=485" className="card-img-top" alt="..." />
-                        <div className="card-body">
-                        <h5 className="card-title">{ title }</h5>
-                        <p className="card-text">{ description }</p>
-                            <a href="/newsdetails" className="btn btn-primary">Go somewhere</a>
-                        </div>
+            <div className='my-3'>
+                <div className="card" style={{ width: "18rem" }}>
+                    <img src={imageUrl} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text">{description}</p>
+                        <a href="/newsdetails" className="btn btn-sm btn-primary">Read More</a>
+                    </div>
                 </div>
             </div>
         )
