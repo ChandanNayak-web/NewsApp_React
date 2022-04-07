d4790af2f837acbd39f20c3b721e32e6ca5f43df
ChandanNayak-web <chandan2000kulu@gmail.com>
2022-04-07 21:21:48 +0530

    here i add the date & time of the news, add the source of the news and convert the time to GMT format

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -8,15 +8,20 @@ export class NewsItem extends Component {
 
     render() {
 
-        let { title, description, imageUrl, newsUrl } = this.props;
+        let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
 
         return (
             <div className='my-3'>
                 <div className="card" >
-                    <img src={!imageUrl?"https://images.hindustantimes.com/img/2022/04/03/1600x900/7ce78bca-849d-49d7-b88c-7cc0b2fddf25_1649002921867_1649002933593.jpg":imageUrl} className="card-img-top" alt="..." />
-                    <div className="card-body">
+                    <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left:'50%' , zIndex:'1'}}>
+                        {source}
+                    </span>
+                    <img src={!imageUrl ? "https://images.hindustantimes.com/img/2022/04/03/1600x900/7ce78bca-849d-49d7-b88c-7cc0b2fddf25_1649002921867_1649002933593.jpg" : imageUrl} className="card-img-top" alt="..." />
+                    <div className="card-body"> fdhdh
                         <h5 className="card-title">{title} </h5>
                         <p className="card-text">{description} </p>
+                        {/* new Date(date).toGMTString() can format the date time */}
+                        <p className="card-text"><small className="text-muted">By {!author ? "Unknown" : author} on {new Date(date).toGMTString()}</small></p>
                         <a href={newsUrl} rel="noreferrer" target='_blank' className="btn btn-sm btn-info">Read More</a>
                     </div>
                 </div>
